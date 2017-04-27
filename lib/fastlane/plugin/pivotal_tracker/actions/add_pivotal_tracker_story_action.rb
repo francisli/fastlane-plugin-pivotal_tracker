b245c23f31ef61d9913aece54889f5dd7c4cbc5e
Francis Li <mail@francisli.com>
2017-04-26 19:17:26 -0700

    Finalize first pass at custom action

--- lib/fastlane/plugin/pivotal_tracker/actions/add_pivotal_tracker_story_action.rb
+++ lib/fastlane/plugin/pivotal_tracker/actions/add_pivotal_tracker_story_action.rb
@@ -1,29 +1,42 @@
 module Fastlane
   module Actions
+    module SharedValues
+      PIVOTAL_TRACKER_STORY_ID = :PIVOTAL_TRACKER_STORY_ID
+    end
+
     class AddPivotalTrackerStoryAction < Action
       def self.run(params)
         api_token = params[:api_token] || ENV["PIVOTAL_TRACKER_API_TOKEN"]
         project_id = params[:project_id] || ENV["PIVOTAL_TRACKER_PROJECT_ID"]
-        UI.message("The add_pivotal_tracker_story plugin is working! #{api_token} #{project_id} #{params.inspect}")
 
-        uri = URI.parse("https://www.pivotaltracker.com/services/v5/projects/#{project_id}/stories")
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
+        response = Helper::PivotalTrackerHelper.get_current_iteration(api_token, project_id)
+        before_id = nil
+        response.first[:stories].each do |story|
+          if story[:current_state] == 'started' || story[:current_state] == 'unstarted'
+            before_id = story[:id]
+            break
+          end
+        end if !response.empty? && response.first[:stories]
 
         data = {}
         data[:name] = params[:name]
-        data[:description] = params[:description] if !params[:description]
-        data[:type] = params[:type] if !params[:type]
+        data[:description] = params[:description] if !params[:description].nil?
+        data[:story_type] = params[:story_type] if !params[:story_type].nil?
+        data[:current_state] = params[:current_state] if !params[:current_state].nil?
+        data[:before_id] = before_id if !before_id.nil?
 
-        response = http.post(uri.request_uri, data.to_json, {
-          'Content-Type': 'application/json',
-          'X-TrackerToken': api_token
-        })
-        UI.message("#{response.inspect}")
+        response = Helper::PivotalTrackerHelper.create_story(api_token, project_id, data)
+        if response[:kind] == 'story'
+          UI.message("Created story [##{response[:id]}]: #{params[:name]}")
+          Actions.lane_context[SharedValues::PIVOTAL_TRACKER_STORY_ID] = response[:id]
+          return Actions.lane_context[SharedValues::PIVOTAL_TRACKER_STORY_ID]
+        else
+          raise Exception
+        end
       end
 
       def self.description
-        "Actions for creating release stories in a Pivotal Tracker project"
+        "Creates a story in a Pivotal Tracker project"
       end
 
       def self.authors
@@ -36,7 +49,7 @@ module Fastlane
 
       def self.details
         # Optional:
-        "Actions for creating release stories in a Pivotal Tracker project from fastlane based on build or version number"
+        "Creates a story in a Pivotal Tracker project"
       end
 
       def self.available_options
@@ -59,8 +72,12 @@ module Fastlane
                                description: "In-depth explanation of the story requirements",
                                   optional: true,
                                       type: String),
-          FastlaneCore::ConfigItem.new(key: :type,
+          FastlaneCore::ConfigItem.new(key: :story_type,
                                description: "Type of story. Valid enumeration values: feature, bug, chore, release",
+                                  optional: true,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :current_state,
+                               description: "Story's state of completion. Valid enumeration values: accepted, delivered, finished, started, rejected, planned, unstarted, unscheduled",
                                   optional: true,
                                       type: String)
         ]
--- lib/fastlane/plugin/pivotal_tracker/helper/pivotal_tracker_helper.rb
+++ lib/fastlane/plugin/pivotal_tracker/helper/pivotal_tracker_helper.rb
@@ -1,11 +1,36 @@
 module Fastlane
   module Helper
+    URL_BASE = "https://www.pivotaltracker.com/services/v5/projects"
+
     class PivotalTrackerHelper
       # class methods that you define here become available in your action
       # as `Helper::PivotalTrackerHelper.your_method`
       #
       def self.show_message
         UI.message("Hello from the pivotal_tracker plugin helper!")
+      end
+
+      def self.get_current_iteration(api_token, project_id)
+        uri = URI.parse("#{URL_BASE}/#{project_id}/iterations?scope=current")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        response = http.get(uri, {
+          'X-TrackerToken': api_token
+        })
+        return JSON.parse(response.body, symbolize_names: true)
+      end
+
+      def self.create_story(api_token, project_id, data)
+        uri = URI.parse("#{URL_BASE}/#{project_id}/stories")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        response = http.post(uri, data.to_json, {
+          'Content-Type': 'application/json',
+          'X-TrackerToken': api_token
+        })
+        return JSON.parse(response.body, symbolize_names: true)
       end
     end
   end
